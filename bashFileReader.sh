22225d278fdc21a4456d84f0de5746cbf79ec4a5
talha-khattak <109832903+talha-khattak@users.noreply.github.com>
2022-09-29 11:08:43 -0400

    Update bashFileReader.sh

--- bashFileReader.sh
+++ bashFileReader.sh
@@ -4,10 +4,10 @@ filenumber=0
 read -p "File Number: " filenumber
 while [[ $filenumber != "0" ]]
   do
-  clear
-  filename="file${filenumber}.txt" 
-  cat $filename
-  echo "\n"
-  filenumber="0"
-  read -p "File Number: " filenumber
+    clear
+    filename="file${filenumber}.txt"
+    cat $filename
+    echo "\n"
+    filenumber="0"
+    read -p "File Number: " filenumber
   done
