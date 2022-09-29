1d53f7f0e27f79f253a2e55cc65148e94b3de0f4
talha-khattak <109832903+talha-khattak@users.noreply.github.com>
2022-09-29 11:05:54 -0400

    Create bashFileReader.sh

---
+++ bashFileReader.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+filenumber=0
+
+read -p "File Number: " filenumber
+while [[ $filenumber != "0" ]]
+  do
+  clear
+  filename="file${filenumber}.txt" 
+  cat $filename
+  echo "\n"
+  filenumber="0"
+  read -p "File Number: " filenumber
+  done
